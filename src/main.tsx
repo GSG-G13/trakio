bb7878582e8ef6fda1058119f63cb863d1a19ca8
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2023-06-19 00:18:55 +0300

    ci: install some dependencies and create theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-  </React.StrictMode>,
+  <h1>Welcome Here, Happy Coding!</h1>,
 )
---
+++ src/theme/index.ts
@@ -0,0 +1,34 @@
+import { createTheme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface PaletteOptions {
+    custom: {
+      background: React.CSSProperties['color'];
+      white: React.CSSProperties['color'];
+      black: React.CSSProperties['color'];
+      purple:  React.CSSProperties['color'];
+    };  
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFDA3C",
+    },
+    secondary: {
+      main: "#343537"
+    },
+    custom:{
+      background: "#1E1F21",
+      white: "#FFFFFF",
+      black: "#000000",
+      purple: "#A9AAC0",
+    },
+  },
+  typography: {
+    fontFamily: ['Montserrat', 'Poppins'].join(','),
+  },
+});
+
+export default theme;
