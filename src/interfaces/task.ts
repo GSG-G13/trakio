6464df75046068c1bb9c2ec10099a5ba803cf8fa
KhaledToha <khaled.f.toha@gmail.com>
2023-06-21 11:47:52 +0200

    feat: create the task card component

---
+++ src/interfaces/task.ts
@@ -0,0 +1,17 @@
+interface task {
+    id?: number;
+    title: string;
+    description: string;
+    created_at: string;
+    due_date: string;
+    project: string;
+    priority: string;
+    color: string;
+    section: string;
+    user_id: number;
+    name: string
+  }
+
+export interface iTask {
+    task: task;
+  }
