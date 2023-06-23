0eb90511e8911636beac34ef76d502578bc03bfb
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-06-23 13:57:46 +0300

    feat: not complete

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,63 @@
+import './footer.css';
+
+const Footer = () => (
+  <div className="footer">
+    <div className="sb__footer section__padding">
+      <div className="sb__footer-links">
+        <div className="sb__footer-links-div">
+          <h4>Company</h4>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+        </div>
+        <div className="sb__fotter-links_div">
+          <h4>Company</h4>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+        </div>
+        <div className="sb__footer-links_div">
+          <h4>Company</h4>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+          <a href="#company">
+            <p>Mission</p>
+          </a>
+        </div>
+        <hr />
+        <div className="sb__footer-below">
+          <div className="sb__footer-copyright">
+            <p>Landing Page Trakio Created By Mohammed Sbeata</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Footer;
