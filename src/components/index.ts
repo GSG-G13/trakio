89003046e89e1aad1b6775727b15ea88c949aa8c
KhaledToha <khaled.f.toha@gmail.com>
2023-06-23 20:53:11 +0300

    fix: resolve conflicts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
+import TaskTable from './TaskTable/TaskTable';
+import TaskRow from './TaskTable/TaskRow';
+import PermanentDrawerLeft from './Layout';
 import TaskCard from './TaskCard';
 
-export default TaskCard;
+export {
+  TaskTable, TaskRow, PermanentDrawerLeft, TaskCard,
+};
