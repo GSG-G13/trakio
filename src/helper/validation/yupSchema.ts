090e21c7b1938a92c7c0cf5f89251ef1bb07b388
ayaaljalees <algaleesaya@gmail.com>
2023-06-23 18:28:43 +0300

    refactor: update validation schema for nada eyes

--- src/helper/validation/yupSchema.ts
+++ src/helper/validation/yupSchema.ts
@@ -1,8 +1,14 @@
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email').required('Email is required'),
-  password: Yup.string().required('Password is required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .matches(/^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid email')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters')
+    .max(50, 'Password must be less than 50 characters')
+    .required('Password is required'),
 });
 
 export default validationSchema;
