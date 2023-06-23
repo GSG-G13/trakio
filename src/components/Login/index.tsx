f281bd78c9706621951a610376cd6b966667835a
ayaaljalees <algaleesaya@gmail.com>
2023-06-23 23:08:10 +0300

    feat: add logo in signin page

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,11 +1,19 @@
+import { CardMedia } from '@mui/material';
 import LoginForm from './LoginForm';
 import {
   BodyContainer, CustomContainer, CustomTypography1,
   CustomTypography2, FormContent, DontHaveAcount, SignupText,
 } from './login.styled';
+import ReactLogo from '../../assets/logo.svg';
 
 const Login = () => (
   <BodyContainer>
+    <CardMedia
+      component="img"
+      alt="Trackio"
+      image={ReactLogo}
+      sx={{ marginTop: '46px', width: '150px', marginLeft: '-251px' }}
+    />
     <CustomContainer>
       <CustomTypography1 variant="h4" gutterBottom>
         Start For Free
