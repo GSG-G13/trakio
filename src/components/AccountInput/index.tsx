a3eebb1a6f104b04ad197687e5ca2eb07cecba18
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-06-24 15:12:41 +0300

    fix: finish account page

--- src/components/AccountInput/index.tsx
+++ src/components/AccountInput/index.tsx
@@ -1,10 +1,10 @@
 import { Box, Typography } from '@mui/material';
-import WrapInput from './acount.styled';
+import { WrapInput } from './acount.styled';
 import THEME from '../../theme';
 
 const AccountInput = ({ label }: {label: string}) => (
   <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-    <Typography color={THEME.palette.primary.main}>
+    <Typography color={THEME.palette.custom.white} sx={{ marginBottom: '1vw' }}>
       {label}
     </Typography>
     <WrapInput />
