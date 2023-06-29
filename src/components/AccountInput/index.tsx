e1a8287939b6c5b5190070023e8d65b7b897dae7
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-06-29 12:28:04 +0300

    feat: get data in account page by cookie

--- src/components/AccountInput/index.tsx
+++ src/components/AccountInput/index.tsx
@@ -2,12 +2,12 @@ import { Box, Typography } from '@mui/material';
 import { WrapInput } from './acount.styled';
 import THEME from '../../theme';
 
-const AccountInput = ({ label }: {label: string}) => (
+const AccountInput = ({ label, value }: {label: string, value: string}) => (
   <Box sx={{ display: 'flex', flexDirection: 'column' }}>
     <Typography color={THEME.palette.custom.white} sx={{ marginBottom: '1vw' }}>
       {label}
     </Typography>
-    <WrapInput />
+    <WrapInput value={value} />
   </Box>
 );
 
--- src/pages/AccountPage/index.tsx
+++ src/pages/AccountPage/index.tsx
@@ -1,38 +1,63 @@
+import { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import { Box, Grid, Typography } from '@mui/material';
 import { AccountInput } from '../../components';
 import THEME from '../../theme';
 import { WrappBtn } from '../../components/AccountInput/acount.styled';
 
-const AccountPage = () => (
-  <Box>
-    <Typography color={THEME.palette.custom.white} sx={{ fontWeight: 'bold', fontSize: '2vw', marginBottom: '2vw' }}>
-      Edit Profile Information
-    </Typography>
-    <Grid container spacing={3} sx={{ marginBottom: '2vw' }}>
-      <Grid item xs={6}>
-        <AccountInput label="Name" />
-      </Grid>
-      <Grid item xs={6}>
-        <AccountInput label="Email" />
-      </Grid>
-      <Grid item xs={6}>
-        <AccountInput label="Old Password" />
-      </Grid>
-      <Grid item xs={6}>
-        <AccountInput label="New Password" />
-      </Grid>
-    </Grid>
-    <Box>
-      <Typography color={THEME.palette.custom.white}>
-        <Typography sx={{ marginBottom: '1vw' }}>
-          Delete Your Account
-        </Typography>
-        <WrappBtn>
-          Delete Account
-        </WrappBtn>
-      </Typography>
-    </Box>
-  </Box>
-);
+interface AccountData {
+  name: string,
+  email: string,
+  phone: string,
+}
 
+const AccountPage = () => {
+  const [name, setUsername] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [phone, setPhone] = useState<string>('');
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    if (token) {
+      // eslint-disable-next-line no-shadow
+      const { name, email, phone } = JSON.parse(atob(token)) as AccountData;
+      setUsername(name);
+      setEmail(email);
+      setPhone(phone);
+    }
+  }, []);
+  return (
+    <Box>
+      <Typography color={THEME.palette.custom.white} sx={{ fontWeight: 'bold', fontSize: '2vw', marginBottom: '2vw' }}>
+        Edit Profile Information
+      </Typography>
+      <Grid container spacing={3} sx={{ marginBottom: '2vw' }}>
+        <Grid item xs={7}>
+          <AccountInput label="Name" value={name} />
+        </Grid>
+        <Grid item xs={7}>
+          <AccountInput label="Email" value={email} />
+        </Grid>
+        <Grid item xs={7}>
+          <AccountInput label="Phone" value={phone} />
+        </Grid>
+        <Grid item xs={7}>
+          <WrappBtn>
+            Save
+          </WrappBtn>
+        </Grid>
+      </Grid>
+      <Box>
+        <Typography color={THEME.palette.custom.white}>
+          <Typography sx={{ marginBottom: '1vw' }}>
+            Delete Your Account
+          </Typography>
+          <WrappBtn>
+            Delete Account
+          </WrappBtn>
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
 export default AccountPage;
