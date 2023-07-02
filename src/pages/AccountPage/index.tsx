794c283c739d313d8314c6814dea996c4636d06b
ayaaljalees <algaleesaya@gmail.com>
2023-07-02 15:29:52 +0300

    feat: link delete account with the backend

--- src/pages/AccountPage/index.tsx
+++ src/pages/AccountPage/index.tsx
@@ -1,38 +1,63 @@
 import { Box, Grid, Typography } from '@mui/material';
-import { AccountInput } from '../../components';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { AccountInput, ErrorAlert } from '../../components';
 import THEME from '../../theme';
 import { WrappBtn } from '../../components/AccountInput/acount.styled';
 
-const AccountPage = () => (
-  <Box>
-    <Typography color={THEME.palette.custom.white} sx={{ fontWeight: 'bold', fontSize: '2vw', marginBottom: '2vw' }}>
-      Edit Profile Information
-    </Typography>
-    <Grid container spacing={3} sx={{ marginBottom: '2vw' }}>
-      <Grid item xs={6}>
-        <AccountInput label="Name" />
-      </Grid>
-      <Grid item xs={6}>
-        <AccountInput label="Email" />
-      </Grid>
-      <Grid item xs={6}>
-        <AccountInput label="Old Password" />
-      </Grid>
-      <Grid item xs={6}>
-        <AccountInput label="New Password" />
-      </Grid>
-    </Grid>
-    <Box>
-      <Typography color={THEME.palette.custom.white}>
-        <Typography sx={{ marginBottom: '1vw' }}>
-          Delete Your Account
+const AccountPage = () => {
+  const navigator = useNavigate();
+  const [openError, setOpenError] = useState(false);
+  const [messageError, setMessageError] = useState('');
+
+  const handleDeleteAccount = () => {
+    axios
+      .delete('/api/account')
+      .then(() => navigator('/login'))
+      .catch((error) => {
+        setOpenError(true);
+        setMessageError(error.response.data.message);
+      });
+  };
+  return (
+    <>
+      <ErrorAlert
+        open={openError}
+        message={messageError}
+        setOpen={setOpenError}
+      />
+      <Box>
+        <Typography color={THEME.palette.custom.white} sx={{ fontWeight: 'bold', fontSize: '2vw', marginBottom: '2vw' }}>
+          Edit Profile Information
         </Typography>
-        <WrappBtn>
-          Delete Account
-        </WrappBtn>
-      </Typography>
-    </Box>
-  </Box>
-);
+        <Grid container spacing={3} sx={{ marginBottom: '2vw' }}>
+          <Grid item xs={6}>
+            <AccountInput label="Name" />
+          </Grid>
+          <Grid item xs={6}>
+            <AccountInput label="Email" />
+          </Grid>
+          <Grid item xs={6}>
+            <AccountInput label="Old Password" />
+          </Grid>
+          <Grid item xs={6}>
+            <AccountInput label="New Password" />
+          </Grid>
+        </Grid>
+        <Box>
+          <Typography color={THEME.palette.custom.white}>
+            <Typography sx={{ marginBottom: '1vw' }}>
+              Delete Your Account
+            </Typography>
+            <WrappBtn onClick={handleDeleteAccount}>
+              Delete Account
+            </WrappBtn>
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+};
 
 export default AccountPage;
