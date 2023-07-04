1ba01d3e99f7b96edcb3b02e97f861f671471f0b
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-07-04 11:35:53 +0300

    fix: update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,18 +1,34 @@
-import { Container, Grid, Typography } from '@mui/material';
+import {
+  Box,
+  CardMedia,
+  Container,
+  Grid,
+  LinearProgress,
+  Typography,
+} from '@mui/material';
+import ReactLogo from '../../assets/logo.svg';
+import THEME from '../../theme';
 
 const Footer = () => (
   <div>
     <Container sx={{ color: 'white' }}>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Typography variant="h4" sx={{ margin: '1vw' }}> Product </Typography>
-          <Typography variant="h6" sx={{ margin: '1vw' }}>
-            {' '}
-            Computer
+          <Typography variant="h4" sx={{ margin: '1vw' }}>
+            <CardMedia
+              component="img"
+              alt="Trackio"
+              image={ReactLogo}
+              sx={{ marginTop: '15px', width: '70%', marginLeft: '9px' }}
+            />
           </Typography>
           <Typography variant="h6" sx={{ margin: '1vw' }}>
             {' '}
-            CPU
+            +62(21)3456 7891
+          </Typography>
+          <Typography variant="h6" sx={{ margin: '1vw' }}>
+            {' '}
+            PT.Trackio Tipes
           </Typography>
           <Typography variant="h6" sx={{ margin: '1vw' }}>
             {' '}
@@ -104,6 +120,18 @@ const Footer = () => (
         </Grid>
       </Grid>
     </Container>
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <LinearProgress sx={{ color: THEME.palette.primary.main, marginTop: '5vh', width: '45vw' }} />
+    </Box>
+    <Typography sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      marginTop: '2vh',
+      color: THEME.palette.primary.main,
+    }}
+    >
+      Landing Page Trackio Created By Team 5
+    </Typography>
   </div>
 );
 
