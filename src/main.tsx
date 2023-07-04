3b2927b788c4eb12195cd6768d619aae03e48ad7
KhaledToha <khaled.f.toha@gmail.com>
2023-07-05 00:06:08 +0300

    feat: add user data by context

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import router from './routes';
 import THEME from './theme';
+import AuthContext from './UserContext/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ThemeProvider theme={THEME}>
-    <RouterProvider router={router} />
+    <AuthContext>
+      <RouterProvider router={router} />
+    </AuthContext>
   </ThemeProvider>,
 );
