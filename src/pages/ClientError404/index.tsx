7133d56f077625bda99fc0912d98e3000916a5b2
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-07-07 12:21:29 +0300

    fix: update client error page

--- src/pages/ClientError404/index.tsx
+++ src/pages/ClientError404/index.tsx
@@ -1,10 +1,12 @@
 import {
   Typography, Container, Divider,
 } from '@mui/material';
+import { Link } from 'react-router-dom';
 import {
   TextContainer,
   AstronautContainer, Image, BigBox,
 } from './client.styled';
+import { WrappBtn } from '../../components/AccountInput/acount.styled';
 
 const ClientError = () => (
   <BigBox>
@@ -13,7 +15,12 @@ const ClientError = () => (
         <Typography variant="h4" component="div" sx={{ color: 'primary.main' }}>ERROR</Typography>
         <Typography variant="h1" component="h1" sx={{ color: 'primary.main', fontSize: '20rem' }}>404</Typography>
         <Divider />
-        <Typography variant="body1" component="div" sx={{ color: 'primary.main' }}>Page Not Found</Typography>
+        <Typography variant="body1" component="div" sx={{ color: 'primary.main' }}>
+          Page Not Found
+          <Link to="/">
+            <WrappBtn sx={{ marginLeft: '1rem' }}> Home Page</WrappBtn>
+          </Link>
+        </Typography>
       </TextContainer>
       <AstronautContainer>
         <Image
