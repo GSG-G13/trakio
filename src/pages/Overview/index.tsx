88ba80a28f08ba9ee7d8513fd5f4d18fdfc74a1b
KhaledToha <khaled.f.toha@gmail.com>
2023-07-07 18:59:56 +0300

    fix: fixing a bug

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/no-array-index-key */
-import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Grid, Typography, Button } from '@mui/material';
 import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { Grid, Typography, Button } from '@mui/material';
 import Lottie from 'react-lottie';
 import AddIcon from '@mui/icons-material/Add';
 import { Member, OverviewTaskCard, TitleAndDesc } from '../../components';
@@ -18,33 +18,44 @@ const Overview = () => {
   const handleOpen = () => setOpen(true);
   const { id } = useParams();
   const [project, setProject] = useState<IProjectDetails>({});
+  const [manager, setManager] = useState<boolean>(false);
   const [sections, setSections] = useState<ISection[]>([]);
   const [tasks, setTasks] = useState<task[]>([]);
   const [members, setMembers] = useState<IMember[]>([]);
   const navigator = useNavigate();
 
   useEffect(() => {
-    axios.get(`/api/project/${id}`).then((res) => {
-      setProject(res.data.data[0]);
-    }).catch((err) => {
-      navigator('/', { state: { error: err.response.data.message } });
-    });
+    axios
+      .get(`/api/project/${id}`)
+      .then((res) => {
+        setProject(res.data.data[0]);
+        setManager(res.data.manager);
+      })
+      .catch((err) => {
+        navigator('/', { state: { error: err.response.data.message } });
+      });
 
     axios.get('/api/sections').then((res) => {
       setSections(res.data.data);
     });
 
-    axios.get(`/api/project/${id}/task`).then((res) => {
-      setTasks(res.data.data);
-    }).catch((err) => {
-      navigator('/', { state: { error: err.response.data.message } });
-    });
+    axios
+      .get(`/api/project/${id}/task`)
+      .then((res) => {
+        setTasks(res.data.data);
+      })
+      .catch((err) => {
+        navigator('/', { state: { error: err.response.data.message } });
+      });
 
-    axios.get(`/api/project/${id}/members`).then((res) => {
-      setMembers(res.data.data);
-    }).catch((err) => {
-      navigator('/', { state: { error: err.response.data.message } });
-    });
+    axios
+      .get(`/api/project/${id}/members`)
+      .then((res) => {
+        setMembers(res.data.data);
+      })
+      .catch((err) => {
+        navigator('/', { state: { error: err.response.data.message } });
+      });
   }, [id, open]);
 
   return (
@@ -83,11 +94,13 @@ const Overview = () => {
           Project Roles
         </Typography>
       </Grid>
-      <Grid marginTop={2} item xs={2}>
-        <Button variant="outlined" onClick={handleOpen} endIcon={<AddIcon />}>
-          Add Members
-        </Button>
-      </Grid>
+      {manager && (
+        <Grid marginTop={2} item xs={2}>
+          <Button variant="outlined" onClick={handleOpen} endIcon={<AddIcon />}>
+            Add Members
+          </Button>
+        </Grid>
+      )}
       <AddMemberModal open={open} handleClose={handleClose} />
       {members.map((item) => (
         <Member member={item} key={item.id} />
