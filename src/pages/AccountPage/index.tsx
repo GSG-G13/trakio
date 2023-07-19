f9c4dd2d2352360fdbc3c1ea0010db1ad16e57b0
KhaledToha <khaled.f.toha@gmail.com>
2023-07-19 20:16:01 +0300

    style: enhancing the style of account page

--- src/pages/AccountPage/index.tsx
+++ src/pages/AccountPage/index.tsx
@@ -2,9 +2,11 @@ import { useState, useContext } from 'react';
 import { Box, Grid, Typography } from '@mui/material';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Lottie from 'react-lottie';
 import { AccountInput, ErrorAlert, ConfirmDialog } from '../../components';
 import { WrappBtn } from '../../components/AccountInput/acount.styled';
 import userContext from '../../UserContext/context';
+import profile from '../../lotties/profile.json';
 
 const AccountPage = () => {
   const navigator = useNavigate();
@@ -35,32 +37,55 @@ const AccountPage = () => {
         setOpen={setOpenError}
       />
       <Box>
-        <Typography sx={{
-          color: 'custom.white',
-          fontWeight: 'bold',
-          fontSize: '2vw',
-          marginBottom: '2vw',
-        }}
+        <Typography
+          sx={{
+            color: 'custom.white',
+            fontWeight: 'bold',
+            fontSize: '2vw',
+            marginY: '2vh',
+          }}
         >
-          Edit Profile Information
+          Profile Information
         </Typography>
-        <Grid container spacing={3} sx={{ marginBottom: '2vw' }}>
-          <Grid item xs={7}>
-            <AccountInput label="Name" value={user?.user?.name} />
+        <Grid container spacing={1} sx={{ marginBottom: '2vw' }}>
+          <Grid item xs={6} display="flex" flexDirection="column" justifyContent="space-evenly">
+            <Grid item xs={12}>
+              <AccountInput label="Name" value={user?.user?.name} />
+            </Grid>
+
+            <Grid item xs={12}>
+              <AccountInput label="Email" value={user?.user?.email} />
+            </Grid>
+            <Grid item xs={12}>
+              <AccountInput label="Phone" value={user?.user?.phone} />
+            </Grid>
           </Grid>
-          <Grid item xs={7}>
-            <AccountInput label="Email" value={user?.user?.email} />
-          </Grid>
-          <Grid item xs={7}>
-            <AccountInput label="Phone" value={user?.user?.phone} />
+          <Grid item marginBottom="2vh" xs={6}>
+            <Lottie
+              height="auto"
+              width={400}
+              options={{
+                animationData: profile,
+                loop: true,
+                autoplay: true,
+                rendererSettings: {
+                  preserveAspectRatio: 'xMidYMid slice',
+                },
+              }}
+            />
           </Grid>
         </Grid>
         <Box>
-          <Typography sx={{ color: 'custom.white' }}>
-            <WrappBtn onClick={() => setConfirmOpen(true)}>
-              Delete Account
-            </WrappBtn>
+          <Typography color="custom.fontGray" width="80%" marginBottom="2vh">
+            We value your control and privacy. By clicking Delete
+            Account you will permanently remove all of your personal data and
+            account information from our system. This action is irreversible,
+            and you will lose access to your account, including any saved
+            preferences, settings, and content
           </Typography>
+          <WrappBtn onClick={() => setConfirmOpen(true)}>
+            Delete Account
+          </WrappBtn>
         </Box>
       </Box>
       <ConfirmDialog
